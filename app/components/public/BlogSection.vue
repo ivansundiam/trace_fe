cc40afa7e686eaa87c92b85a052ab4820d9def64
ivansundiam <sundiam.patrickivan.12032002@gmail.com>
2026-01-01 13:58:29 +0800

    feat: blog section

---
+++ app/components/public/BlogSection.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+  import type { SegmentButton } from '../SegmentButtons.vue';
+
+  const segmentButtons: SegmentButton[] = [
+    { name: 'All', value: 'all', },
+    { name: 'Destination', value: 'destination', },
+    { name: 'Culinary', value: 'culinary', },
+    { name: 'Lifestyle', value: 'lifestyle', },
+    { name: 'Tips & Hacks', value: 'tips', },
+  ];
+
+  const selectedTab = ref<string>('all');
+
+</script>
+
+<template>
+  <div class="max-w-7xl mx-auto py-8">
+    <h1 class="title">Blog</h1>
+    <p>Here, we share travel tips, destination guides, and stories that inspire youre next adventure.</p>
+
+    <div class="my-3 flex justify-between">
+      <SegmentButtons :items="segmentButtons" v-model="selectedTab"  />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  @reference 'tailwindcss';
+  /* @reference '~/assets/css/app.css'; */
+
+  .title {
+    @apply text-2xl md:text-3xl;
+  }
+
+</style>
--- app/pages/(public)/index.vue
+++ app/pages/(public)/index.vue
@@ -6,8 +6,10 @@
 
 <template>
   <PublicHero />
+
+  <PublicBlogSection />
 </template>
 
 <style scoped>
-
+  
 </style>
