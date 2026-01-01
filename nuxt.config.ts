817c5070f5d6f6632e472a4f340981070befb614
ivansundiam <sundiam.patrickivan.12032002@gmail.com>
2026-01-01 21:23:52 +0800

    feat: config api base

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,12 +5,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['./app/assets/css/app.css'],
-
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
-
   modules: ['@nuxt/icon', '@pinia/nuxt'],
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api',
+    }
+  },
 })
