5985a71dd47b4a1f9d5ba2f0f526a47d5a0ed428
ivansundiam <sundiam.patrickivan.12032002@gmail.com>
2026-01-01 13:33:54 +0800

    ref: relocate styling to global btn

--- app/components/ButtonComponent.vue
+++ app/components/ButtonComponent.vue
@@ -2,18 +2,19 @@
   withDefaults(
       defineProps<{
         text?: string;
-        variant?: 'primary' | 'secondary' | 'success' | 'warning' | 'danger' | 'dark';
+        variant: 'primary' | 'secondary' | 'success' | 'warning' | 'danger' | 'dark';
         size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
         radius?: 'sm' | 'md' | 'lg' | 'full'
       }>(), {
+        variant: 'secondary',
         size: 'md',
-        radius: "md"
+        radius: "md",
       }
   )
 </script>
 
 <template>
-  <button :class="['btn-root', variant, size, `rounded-${radius}`]">
+  <button :class="['btn', `btn-${variant}`, size, `rounded-${radius}`]">
     <span v-if="text">{{ text }}</span>
     <slot v-else ></slot>
   </button>
@@ -22,38 +23,6 @@
 <style scoped>
   @reference "tailwindcss";
   @reference "~/assets/css/app.css";
-
-  .btn-root {
-    @apply rounded-md bg-primary transition-all hover:bg-primary-hover active:bg-primary-active cursor-pointer;
-  }
-
-
-  /* variants */
-  .primary {
-    @apply bg-primary hover:bg-primary-hover active:bg-primary-active text-black;
-  }
-
-  .secondary {
-    @apply bg-transparent hover:bg-primary-hover active:bg-primary-active text-inherit hover:text-black;
-  }
-
-  .success {
-    @apply bg-success hover:bg-success-hover active:bg-success-active;
-  }
-
-  .danger {
-    @apply bg-danger hover:bg-danger-hover active:bg-danger-active;
-  }
-
-  .warning {
-    @apply bg-warning hover:bg-warning-hover active:bg-warning-active;
-  }
-  
-  .dark {
-    @apply bg-primary-dark hover:bg-primary-dark-hover active:bg-primary-dark-active text-white;
-  }
-
-  /* sizes */
 
   .sm {
     @apply px-3 py-1;
