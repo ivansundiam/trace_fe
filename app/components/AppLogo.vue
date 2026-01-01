15dc53b4c360dca57a326436d07b905a8554d83a
ivansundiam <sundiam.patrickivan.12032002@gmail.com>
2026-01-01 13:34:23 +0800

    chore: update font brand util name

--- app/components/AppLogo.vue
+++ app/components/AppLogo.vue
@@ -8,7 +8,7 @@
 
 <template>
   <NuxtLink to="/" class="flex items-center gap-1 max-w-64">
-    <Icon name="ion:compass-outline" :size /> <span class="text-lg font-bold font-heading">Trace</span>
+    <Icon name="ion:compass-outline" :size /> <span class="text-lg font-bold font-brand">Trace</span>
   </NuxtLink>
 </template>
 
