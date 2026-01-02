e60df72c922de38af4c0437b8d3581fb28002c17
ivansundiam <sundiam.patrickivan.12032002@gmail.com>
2026-01-02 22:11:07 +0800

    npm: '@nuxt/ui'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
       tailwindcss(),
     ],
   },
-  modules: ['@nuxt/icon', '@pinia/nuxt'],
+  modules: ['@nuxt/icon', '@nuxt/ui', '@pinia/nuxt'],
   runtimeConfig: {
     public: {
       apiBase: '/api',
