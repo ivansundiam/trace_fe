b4f5148f397b26f66ffece3e4c16280a0ccaa456
ivansundiam <sundiam.patrickivan.12032002@gmail.com>
2026-01-02 22:05:43 +0800

    ref: relocate loading of user to plugin

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -3,11 +3,11 @@ import { useAuthStore } from "~/stores/auth/authStore"
 export default defineNuxtRouteMiddleware(async (to) => {
     const auth = useAuthStore();
 
-    if(!auth.isLoggedIn){
-        try {
-           await auth.loadUser();           
-        } catch (error) {
-            if(to.meta.auth) return navigateTo('/login');
-        }
+    if (to.meta.auth === true && !auth.isLoggedIn) {
+        return navigateTo('/login');
+    }
+
+    if (to.path === '/login' && auth.isLoggedIn) {
+        return navigateTo('/');
     }
 })
--- app/stores/auth/authStore.ts
+++ app/stores/auth/authStore.ts
@@ -2,48 +2,73 @@ import type { User } from "~/shared/types/user";
 
 export const useAuthStore = defineStore('auth', () => {
   const http = useHttp('auth');
-
-  //STATE
   const currentUser = ref<User | null>(null);
+  const loading = ref<boolean>(false);
+  const resolved = ref<boolean>(false);
+  const error = ref<any | null>(null);
 
-  //GETTERS
   const isLoggedIn = computed<boolean>(() => !!currentUser.value)
 
-  // ACTIONS
   function setUser(value: User) {
     currentUser.value = value
   }
 
-  async function loadUser() {
+  function resetUser() {
+    currentUser.value = null;
+  }
+
+  async function loadUser(): Promise<User | null> {
     try {
       const user = await http<User>('user');
       setUser(user);
       return user;
-    } catch (error) {
-      console.log(error);
+    } catch (err: any) {
+      resetUser();
+      error.value = err;
+      console.log(error.value);
+      
+      return null;
+    } finally {
+      resolved.value = true;
     }
   }
 
   async function login(email: string, password: string) {
     try {
+      loading.value = true
       const user = await http<User>('login', {
         method: 'POST',
         body: { email, password },
       });
-  
+
       setUser(user);
       navigateTo('/')
-    } catch (error) {
-      console.log(error);
-    
+    } catch (err: any) {
+      error.value = err.error.message;
+    } finally {
+      loading.value = false
+    }
+  }
+
+  async function logout() {
+    try {
+      loading.value = true
+      await http('logout', { method: 'POST' });
+      resetUser();
+      navigateTo('/login');
+    } finally {
+      loading.value = false
     }
   }
 
   return {
     currentUser,
+    isLoggedIn,
+    loading,
+    resolved,
     setUser,
     login,
     loadUser,
-    isLoggedIn
+    logout
   }
 });
